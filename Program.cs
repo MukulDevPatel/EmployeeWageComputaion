2ace1cf20e0fdab54e58ff28770745a4eaf046ed
MukulDevPatel <patelmukuldev@gmail.com>
2023-04-26 17:49:51 +0530

    [Mukul Dev] Add : Greeting for employee wage computaion problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputaion
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computaion program");
+        }
+    }
+}
