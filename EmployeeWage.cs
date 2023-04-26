aa74a806e2306e91d93cc81ae84c3bfba8e0a7f6
MukulDevPatel <patelmukuldev@gmail.com>
2023-04-26 18:31:35 +0530

    [Mukul Dev] Add: Calculate daily wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -10,17 +10,20 @@ namespace EmployeeWageComputaion
     {
         public static void EmpAttendance()
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+            int empHour = 0, empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHour = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHour = 0;
             }
+            empWage = empHour * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
