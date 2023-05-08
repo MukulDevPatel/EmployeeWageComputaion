c0b0ba27bf658663c5b49bb9ed3a12ed792501fb
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-08 16:21:45 +0530

    [Mukul Dev] Add: Refactor the code and use computeEmpWage method

---
+++ EmployeeWage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputaion
+{
+    public class EmployeeWage
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int TOTAL_WORKING_DAYS = 20;
+        public const int TOTAL_WORKING_HRS = 100;
+        public static int computeEmpWage()
+        {
+            //Variables
+            int empHours = 0;
+            int totalEmpHrs = 0, totalWorkindDays = 0;
+
+            //Computation
+            while (totalEmpHrs <= TOTAL_WORKING_HRS && totalWorkindDays < TOTAL_WORKING_DAYS)
+            {
+                totalWorkindDays++;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                totalEmpHrs += empHours;
+                Console.WriteLine("Day#: " + totalWorkindDays + " Emp Hrs: " + empHours);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace EmployeeWageComputaion
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computaion program");
+
+            EmployeeWage.computeEmpWage();
         }
     }
 }
