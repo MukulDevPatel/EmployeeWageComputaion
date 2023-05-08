1501d910ef2c7b8a626ee2c3af4742af11f4f553
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-08 21:24:03 +0530

    [Mukul Dev] Add: Calculate total Emp Wage for each company

--- EmpWageBuilder.cs
+++ EmpWageBuilder.cs
@@ -6,23 +6,35 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputaion
 {
-    public class EmployeeWage
+    public class EmpWageBuilder
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        
-        public static int computeEmpWage(string company,int empRateperHour,int numOfWorkingDays,int maxHoursPerMonth)
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        public EmpWageBuilder(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
         {
             //Variables
             int empHours = 0;
             int totalEmpHrs = 0, totalWorkindDays = 0;
 
             //Computation
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkindDays < numOfWorkingDays)
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkindDays < this.numOfWorkingDays)
             {
                 totalWorkindDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0,3);
+                int empCheck = random.Next(0, 3);
 
                 switch (empCheck)
                 {
@@ -39,9 +51,12 @@ namespace EmployeeWageComputaion
                 totalEmpHrs += empHours;
                 Console.WriteLine("Day#: " + totalWorkindDays + " Emp Hrs: " + empHours);
             }
-            int totalEmpWage = totalEmpHrs * empRateperHour;
-            Console.WriteLine("Total Emp Wage for company: " + company+ " is: " + totalEmpWage);
-            return totalEmpWage;
+            int totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company: " + company + " is: " + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for Company: " + this.company + " is: " + this.totalEmpWage;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace EmployeeWageComputaion
         {
             Console.WriteLine("Welcome to Employee Wage Computaion program");
 
-            EmployeeWage.computeEmpWage("DMart",20,2,10);
-            EmployeeWage.computeEmpWage("Reliance", 20, 4, 10);
+            EmpWageBuilder dMart=new EmpWageBuilder("DMart", 20, 2, 10);
+            EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 20, 4, 10);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
         }
     }
 }
